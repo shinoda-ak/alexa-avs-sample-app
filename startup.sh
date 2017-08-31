b265c23f51639a773060150fa8f8a9497c23cb8e
shinoda.hg <shinoda.hg@om.asahi-kasei.co.jp>
2017-08-31 11:30:39 +0900

    Specify alpn-booot version for javaclient

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
+cd /home/pi/alexa-avs-sample-app
 cd samples/
 cd tLED/
 lxterminal -e "sudo python tLEDServer.py" &
 cd ../companionService
 lxterminal -e "npm start" &
 cd ../javaclient
-lxterminal -e "mvn exec:exec" &
-#sleep 32
+lxterminal -e "mvn exec:exec -Dalpn-boot.version=8.1.6.v20151105" &
+sleep 32
 #Uncomment if run automatically at startup
 cp ~/leftarc ~/.asoundrc
 sleep 1
